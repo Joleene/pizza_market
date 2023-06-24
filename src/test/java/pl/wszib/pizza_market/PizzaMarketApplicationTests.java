7824af4bbd640c7fa79dd492acd6962b4a1ac4e2
Joleene <joanna.lawinska1994@gmail.com>
2023-06-24 13:11:05 +0200

    Strona główna

---
+++ src/test/java/pl/wszib/pizza_market/PizzaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wszib.pizza_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
