e4f4c540126a897fb4a64cdd1fa44b9673c0f5df
Joleene <joanna.lawinska1994@gmail.com>
2023-07-04 19:55:02 +0200

    Poprawki

--- src/main/java/pl/wszib/pizza_market/web/controllers/OpinionsController.java
+++ src/main/java/pl/wszib/pizza_market/web/controllers/OpinionsController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.wszib.pizza_market.services.OpinionsService;
 import pl.wszib.pizza_market.web.models.OpinionsModel;
